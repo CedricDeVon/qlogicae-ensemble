7aa8a4b67fb352dd12eaa21d9bc52cae0232c713
CedricDeVon <vaughncedriclipardoaraneta@gmail.com>
2026-09-22 20:24:52 +0800

    updated configurations

---
+++ .qlogicae/logis/v3/public/plugin/custom.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import Any
+
+import time
+
+def run_command(**kwargs: Any) -> Any:
+    if not kwargs:
+        return 0
+
+    a = (kwargs.get("a", 0) or 0)
+    b = (kwargs.get("b", 0) or 0)
+
+    return a + b
+
+
+def run_time_now() -> Any:
+    return f"{time.time_ns()}"
+
+
+command = {
+    "public-a": {
+        "value": run_command
+    }
+}
+
+macros = {
+    "static": {
+        "file": {
+            "targets": {
+                "public-a": {
+                    "value": "public | ${{ qlogicae-logis-base-description }} | ${{ public-time-now }} | ${{ public-time-now }}"
+                },
+                "public-b": {
+                    "value": "public-a | ${{ public-a }}"
+                }
+            }
+        }
+    },
+    "dynamic": {
+        "targets": {
+            "public-time-now": {
+                "value": run_time_now
+            }
+        }
+    }
+}
+
